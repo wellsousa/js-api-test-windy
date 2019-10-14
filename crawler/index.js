833d16c2b00f1cdd8fde9319558dfef4970184af
Wellington de Sousa <wellsousa1987@outlook.com>
2019-10-14 16:51:50 -0300

    Criando um crawler para obter informaçoes relevantes

---
+++ crawler/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+var data = require('./windyScrap');
+
+const app = express()
+
+app.listen(3000, function(req, res){
+    console.log('crawler running at port 3000');
+    console.log(data);
+});
---
+++ crawler/windyScrap.js
@@ -0,0 +1,11 @@
+
+const request = require('request')
+const cheerio = require('cheerio')
+
+
+
+var data= {
+    value: true
+}
+
+module.exports = data
